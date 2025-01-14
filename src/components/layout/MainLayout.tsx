55b85e6e57058bac7ca52d46382430b63b5f0002
thkiaAdiba <athkiaadiba@gmail.com>
2025-01-14 11:13:20 +0600

    added part-3 of phum

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,8 @@
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/hooks";
+import { logOut } from "../../redux/features/auth/authSlice";
 
 const { Header, Content } = Layout;
 
@@ -30,11 +32,19 @@ const { Header, Content } = Layout;
 // ];
 
 const MainLayout = () => {
+  const disPatch = useAppDispatch();
+
+  const handleLogout = () => {
+    disPatch(logOut());
+  };
+
   return (
     <Layout style={{ height: "100vh" }}>
       <Sidebar></Sidebar>
       <Layout>
-        <Header style={{ padding: 0 }} />
+        <Header>
+          <Button onClick={handleLogout}>Logout</Button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,41 +1,55 @@
-import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { Button, Row } from "antd";
+import { FieldValues, useForm, useFormContext } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import PHFrom from "../components/form/PHFrom";
+import PHInput from "../components/form/PHInput";
 
 const Login = () => {
+  const navigate = useNavigate();
   const disPatch = useAppDispatch();
-  const { register, handleSubmit } = useForm();
-  const [login, { error }] = useLoginMutation();
+  // const { register, handleSubmit } = useForm();
+  const [login] = useLoginMutation();
 
-  const onSubmit = async (data) => {
+  const defaultValues = {
+    id: "A-0001",
+    password: "admin1",
+  };
+
+  const onSubmit = async (data: FieldValues) => {
     // console.log(data);
+    const toastId = toast.loading("Logging in", { duration: 2000 });
 
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
 
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken) as TUser;
 
-    disPatch(setUser({ user: user, token: res.data.accessToken }));
+      disPatch(setUser({ user: user, token: res.data.accessToken }));
+      toast.success("Logged in successfully!", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      toast.error("Something went wrong!", { id: toastId, duration: 2000 });
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">ID:</label>
-        <input type="text" id="id" {...register("id")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PHFrom onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="id" label="ID:" />
+        <PHInput type="text" name="password" label="Password:" />
+        <Button htmlType="submit">Login</Button>
+      </PHFrom>
+    </Row>
   );
 };
 
